f7ccf201ca25f47b46a37f5aaabc8b51fc826a2d
Sarahodv <47123648+Sarahodv@users.noreply.github.com>
2020-06-08 11:28:10 +0200

    Update TD2-s1.py

--- TD2/TD2-s1.py
+++ TD2/TD2-s1.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Dec  4 10:34:39 2018
+Created on Tue Apr  24 10:34:39 2019
 
 @author: sarahoterodelval
 """
 import http.server
 import socketserver
-# définition du nouveau handler
-class RequestHandler(http.server.SimpleHTTPRequestHandler): # sous-répertoire racine des documents statiques
+# définition du nouveau handler
+class RequestHandler(http.server.SimpleHTTPRequestHandler): # sous-répertoire racine des documents statiques
     static_dir = '/client'
-# on surcharge la méthode qui traite les requêtes GET
+# on surcharge la méthode qui traite les requêtes GET
     def do_GET(self):
-# on modifie le chemin d'accès en insérant un répertoire préfixe
+# on modifie le chemin d'accès en insérant un répertoire préfixe
         self.path = self.static_dir + self.path
-# on traite la requête via la classe parent
+# on traite la requête via la classe parent
         http.server.SimpleHTTPRequestHandler.do_GET(self)
         
 # instanciation et lancement du serveur
